d9b5e53d260a80dcec3cc744b983b5cc2a9d5b03
Aira <aira.a0001@gmail.com>
2022-03-11 14:48:11 +0800

    nodeMcu ups

--- app/Http/Controllers/NodeMcuController.php
+++ app/Http/Controllers/NodeMcuController.php
@@ -15,37 +15,44 @@ class NodeMcuController extends Controller
         
         $user = User::where('rfid_number', $request->rfid_number)->exists();
         // $slot = ParkingSlot::where('user_id', $user->id)->first();
-        // $dt = Carbon::now();
-        // $todayDate = $dt->toDateString();
+        $dt = Carbon::now();
+        $todayDate = $dt->toDateString();
 
 
         if($user){
             $user = User::where('rfid_number', $request->rfid_number)->first();
-            ParkingSlot::where('user_id', $user->id)
-            ->update([
+            $slot = ParkingSlot::where('user_id', $user->id)->exists();
+            if($slot){
+                ParkingSlot::where('user_id', $user->id)
+                ->update([
                 'user_id' => null,
                 'status' => ParkingSlot::AVAILABLE,
-            ]);
-            // Log::create([
-            //     'name_of_user'=>$user->name,
-            //     'number_of_slot'=>$slot->parking_number,
-            //     'description'=>'Exited',
-            //     'date_time'=>$todayDate,
-            // ]);
+                ]);
+                Log::create([
+                    'name_of_user'=>$user->name,
+                    'number_of_slot'=>$slot->parking_number,
+                    'description'=>'Exited',
+                    'date_time'=>$todayDate,
+                ]);
+            }
+            else{
+                Log::create([
+                    'name_of_user'=>$user->name,
+                    'number_of_slot'=>$slot->parking_number,
+                    'description'=>'Entered',
+                    'date_time'=>$todayDate,
+                ]);
+            }
+            // 
 
-            Reservation::whereDate('date',Carbon::now())->where('user_id', $user->id)->delete();
+            // Reservation::whereDate('date',Carbon::now())->where('user_id', $user->id)->delete();
 
             return [
                 "authorized"=>"Autorized User"
             ];
         }
 
-        // Log::create([
-        //     'name_of_user'=>$user->name,
-        //     'number_of_slot'=>$slot->parking_number,
-        //     'description'=>'Entered',
-        //     'date_time'=>$todayDate,
-        // ]);
+        
 
        
     }
