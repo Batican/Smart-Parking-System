902e612b40afcca5cace9e5db2d83b6fe0e895a1
Aira <aira.a0001@gmail.com>
2022-03-24 20:27:04 +0800

    update

--- resources/js/pages/Backoffice/Reservations.vue
+++ resources/js/pages/Backoffice/Reservations.vue
@@ -13,11 +13,11 @@
                         class="elevation-1"
                 >   
                     <template v-slot:item.start_time ="{ item }">
-                        {{moment(start_time).format('hh:mm a')}}
+                        {{moment(item.start_time).format('hh:mm a')}}
                     </template>
 
                     <template v-slot:item.end_time ="{ item }">
-                        {{moment(end_time).format('hh:mm a')}}
+                        {{moment(item.end_time).format('hh:mm a')}}
                     </template>
 
                     <template v-slot:item.status ="{ item }">
