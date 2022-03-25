df70ec4076191daaf4d0c89a0651c1a750f0b075
Aira <aira.a0001@gmail.com>
2022-03-25 16:31:59 +0800

    controller update

--- app/Http/Controllers/NodeMcuController.php
+++ app/Http/Controllers/NodeMcuController.php
@@ -23,17 +23,18 @@ class NodeMcuController extends Controller
             $user = User::where('rfid_number', $request->rfid_number)->first();
             $slot = ParkingSlot::where('user_id', $user->id)->exists();
             if($slot){
-                $user_slot = ParkingSlot::where('user_id', $user->id)
-                ->update([
-                'user_id' => null,
-                'status' => ParkingSlot::AVAILABLE,
-                ]);
+                $user_slot = ParkingSlot::where('user_id', $user->id)->first();
                 Log::create([
                     'name_of_user'=>$user->name,
                     'number_of_slot'=>$user_slot->parking_number,
                     'description'=>'Exited',
                     'date_time'=>$todayDate,
                 ]);
+
+                $user_slot->update([
+                    'user_id' => null,
+                    'status' => ParkingSlot::AVAILABLE,
+                    ]);
             }
             else{
                 Log::create([
